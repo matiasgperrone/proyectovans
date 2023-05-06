f32244e698a2357eba13a8c8f1ad59d7f27e1cd9
mgperrone <matias.perrone.eis@gmail.com>
2023-05-06 14:35:27 -0300

    Commit antes de JavaScript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const productosArray = [
+	{
+		id: "zapatillas-01",
+		titulo: "Zapatillas 01",
+		imagen: "./",
+		categoria: {
+			nombre: "Zapatillas",
+			id: "zapatillas",
+				}
+		precio: 26600
+	}
+
+]
